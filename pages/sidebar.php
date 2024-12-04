c5c51442dcb9ce213795189b041fed325c357920
Kiran dhoke <dhokekiran98@gmail.com>
2024-12-05 04:34:21 +0530

    removed unused css and also added bg color on element in sidebar

--- pages/sidebar.php
+++ pages/sidebar.php
@@ -6,10 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/ed4167ae3c.js" crossorigin="anonymous"></script>
     <title>Document</title>
+    <style>
+        .active {
+            background-color: #f0f0f0;
+            border-left: 4px solid #000;
+        }
+    </style>
 </head>
 
 <body>
-    <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-radius-lg fixed-start ms-2  bg-white my-2" id="sidenav-main">
+    <?php
+    // Get the current file name
+    $current_page = basename($_SERVER['REQUEST_URI']);
+    ?>
+    <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-radius-lg fixed-start ms-2 bg-white my-2" id="sidenav-main">
         <div class="sidenav-header">
             <i class="fas fa-times p-3 cursor-pointer text-dark opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
             <a class="navbar-brand px-4 py-3 m-0" href="../pages/index.php" target="_blank">
@@ -18,16 +28,16 @@
             </a>
         </div>
         <hr class="horizontal dark mt-0 mb-2">
-        <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+        <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="../pages/index.php">
+                    <a class="nav-link text-dark <?= ($current_page == 'index.php') ? 'active' : ''; ?>" href="../pages/index.php">
                         <i class="material-symbols-rounded opacity-5">dashboard</i>
                         <span class="nav-link-text ms-1">Dashboard</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="../pages/tables.php">
+                    <a class="nav-link text-dark <?= ($current_page == 'tables.php') ? 'active' : ''; ?>" href="../pages/tables.php">
                         <i class="material-symbols-rounded opacity-5">table_view</i>
                         <span class="nav-link-text ms-1">Tables</span>
                     </a>
@@ -36,29 +46,16 @@
                     <h6 class="ps-4 ms-2 text-uppercase text-xs text-dark font-weight-bolder opacity-5">Account pages</h6>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="../pages/profile.php">
+                    <a class="nav-link text-dark <?= ($current_page == 'profile.php') ? 'active' : ''; ?>" href="../pages/profile.php">
                         <i class="material-symbols-rounded opacity-5">person</i>
                         <span class="nav-link-text ms-1">Profile</span>
                     </a>
                 </li>
                 <li class="nav-item" id="login-Div">
-
                 </li>
-                <!-- <li class="nav-item">
-                    <a class="nav-link text-dark" href="../pages/sign-in.php">
-                        <i class="material-symbols-rounded opacity-5">login</i>
-                        <span class="nav-link-text ms-1">Sign In</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-dark" href="../pages/sign-up.php">
-                        <i class="material-symbols-rounded opacity-5">assignment</i>
-                        <span class="nav-link-text ms-1">Sign Up</span>
-                    </a>
-                </li> -->
             </ul>
         </div>
-        <div class="sidenav-footer position-absolute w-100 bottom-0 ">
+        <div class="sidenav-footer position-absolute w-100 bottom-0">
             <div class="mx-3">
                 <p>Made By <b style="color: black;">Kiran Dhoke</b></p>
             </div>
